1c64456e526d1779d9cf7d301742329c368dc38a
Bedinah <bernabazubagira@gmail.com>
2026-03-31 18:17:27 +0200

    implement personal portofolio in html/css

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const options = {
+    strings: ["UI/UX Designer - Full Stack Developer"],
+    typeSpeed: 60,
+    backSpeed: 30,
+    startDelay: 500,
+    showCursor: true,
+    cursorChar: "|",
+    loop: false,
+  };
+
+  // Initialize Typed.js on the #typed-text element
+  const typed = new Typed("#typed-text", options);
+});
